c0c9002674a4ebcc743506134dbf893e3265048a
Ubuntu <ubuntu@ip-172-31-40-121.us-west-2.compute.internal>
2013-07-10 21:54:11 +0000

    Require fs

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync("index.html");
-var text = buffer.toString();
+var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(text);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
